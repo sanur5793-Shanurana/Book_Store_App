ec7a4fec6fcd5f782d654ce5f6ac731fb4820e52
sanur5793-Shanurana <sanur5793@gmail.com>
2025-11-24 01:05:20 +0530

    This my first commit of this project i made a home page with a navbar or middle content and a footer also where can we see free books

---
+++ src/components/Cards.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+function Cards({ item }) {
+  console.log(item);
+  return (
+    <>
+      <div>
+        <div className="card bg-base-100 w-96 shadow-sm">
+          <figure>
+            <img
+              src={item.image}
+              alt="Shoes" />
+          </figure>
+          <div className="card-body">
+            <h2 className="card-title">
+              Card Title
+              <div className="badge badge-secondary">NEW</div>
+            </h2>
+            <p>A card component has a figure, a body part, and inside body there are title and actions parts</p>
+            <div className="card-actions justify-end">
+              <div className="badge badge-outline">Fashion</div>
+              <div className="badge badge-outline">Products</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Cards;
